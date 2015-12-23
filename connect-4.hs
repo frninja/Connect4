7946308e1d9a16be357194bfa1672cc5b90a7cb4
Oleg Batashov <krash.se.us@gmail.com>
2015-12-23 21:18:53 +0300

    Forgotten GameState prototyped

--- connect-4.hs
+++ connect-4.hs
@@ -12,8 +12,13 @@ gameFieldWidth  = 7
 gameFieldHeight = 6
 
 {- Field cell -}
-data FieldCell = Empty | Player | AI
+data FieldCell = EmptyField | PlayerField | AiField
     deriving (Show, Eq)
 
-type GameField = [[FieldCell]]
+type Field = [[FieldCell]]
 
+{- Turn -}
+data Turn = PlayerTurn | AiTurn
+
+{- Game State -}
+data GameState = GameState Field Turn
