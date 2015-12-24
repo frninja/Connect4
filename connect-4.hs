60c21d21ad48d9e9d479dd4e6d52481716dd9175
IrinaPrikhodko <rokshata@yandex.ru>
2015-12-24 21:41:15 +0300

    Winner

--- connect-4.hs
+++ connect-4.hs
@@ -17,6 +17,9 @@ gameFieldHeight = 6
 {- Field cell -}
 data FieldCell = EmptyCell | PlayerCell | AiCell
     deriving (Show, Eq)
+	
+data Winner = AI | Player | Draw
+	deriving (Show, Eq)
 
 type Field = [[FieldCell]]
 
@@ -55,6 +58,33 @@ makeMove gs@(GameState field currentTurn) (Just moveCol)
         post = reverse $ take (gameFieldWidth - moveCol - 1) $ reverse field
         newCol = [(replicate (gameFieldHeight - notEmptyCount - 1) EmptyCell)]
 
-winner :: GameState -> Maybe FieldCell
-winner = undefined
+winner :: GameState -> Maybe Winner
+winner (GameState field _) 
+    | horisMin <= -4 = Just AI
+    | vertMin <= -4 = Just AI
+    | leftUpRightDownMin <= -4 = Just AI
+    | leftDownRightUpMin <= -4 = Just AI
+    | horisMax >= 4 = Just Player
+    | vertMax >= 4 = Just Player
+    | leftUpRightDownMax >= 4 = Just Player
+    | leftDownRightUpMax >= 4 = Just Player
+    | allNotEmpty field = Just Draw
+    | otherwise = Nothing
+    where
+	    
+        (horisMin, horisMax) = minMaxFind $ concatMap (map msum) $ map group $ transpose field
+        (vertMin, vertMax) = minMaxFind $ concatMap (map msum) $ map group field
+        (leftDownRightUpMin, leftDownRightUpMax) = minMaxFind $ concatMap (map msum) $ map group $ transpose $ skew field
+        (leftUpRightDownMin, leftUpRightDownMax) =  minMaxFind $ concatMap (map msum) $ map group $ transpose $ skew $ reverse field 
 
+allNotEmpty :: Field -> Bool
+allNotEmpty = undefined
+
+msum :: [FieldCell] -> Int
+msum = undefined
+
+minMaxFind :: (Ord a, Eq a)=>[a]->(a,a)
+minMaxFind = undefined
+
+skew :: Field -> Field
+skew = undefined
